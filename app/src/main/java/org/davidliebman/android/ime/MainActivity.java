c3c48b31dac599d2f64db5caef9f2348416ead46
David Liebman <radiodee1@gmail.com>
2016-02-06 09:37:20 -0500

    move Operation to AsyncTask

--- app/src/main/java/org/davidliebman/android/ime/MainActivity.java
+++ app/src/main/java/org/davidliebman/android/ime/MainActivity.java
@@ -45,24 +45,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         view = new InnerView(this);
-        //setContentView((View) view);
-        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        //setSupportActionBar(toolbar);
+
+
 
         FrameLayout screenLoc = (FrameLayout) findViewById(R.id.innerView);
         screenLoc.addView(view);
         //screenLoc.addView(new InnerView(this));
 
-        /*
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        */
 
 
 
@@ -138,6 +127,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 clearScreen();
+                mExampleBlockOutput = false;
             }
         });
     }
